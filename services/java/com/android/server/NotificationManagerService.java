d3c933979f068f57eb42738c93a7bbc77948a045
cj360 <ayunker551@gmail.com>
2014-06-22 17:05:54 -0400

    Tweak NotifcationManagerService
    -Add mWasScreenOn boolean to check if screen is on, and then if the user
    is in the color picker for cutom led force update to preview selected
    led color with chosen lengh & speed.
    
    Change-Id: Ic9e1b67f9e8e50f21495555ef9f253bf68713e7b
    Signed-off-by: cj360 <ayunker551@gmail.com>

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -174,6 +174,7 @@ public class NotificationManagerService extends INotificationManager.Stub
 
     // for enabling and disabling notification pulse behavior
     private boolean mScreenOn = true;
+    private boolean mWasScreenOn = true;
     private boolean mDreaming = false;
     private boolean mInCall = false;
     private boolean mNotificationPulseEnabled;
@@ -1464,6 +1465,8 @@ public class NotificationManagerService extends INotificationManager.Stub
                 mScreenOn = true;
             } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
                 mScreenOn = false;
+                mWasScreenOn = true;
+                updateLightsLocked();
                 updateNotificationPulse();
             } else if (action.equals(Intent.ACTION_DREAMING_STARTED)) {
                 mDreaming = true;
@@ -2566,7 +2569,18 @@ public class NotificationManagerService extends INotificationManager.Stub
         }
 
         // Don't flash while we are in a call or screen is on
-        if (mLedNotification == null || mInCall || mScreenOn || !mNotificationPulseEnabled) {
+        final boolean enableLed;
+        if (mLedNotification == null) {
+            enableLed = false;
+        } else if (isLedNotificationForcedOn(mLedNotification)) {
+            enableLed = true;
+        } else if (mInCall || (mScreenOn && !mDreaming)) {
+            enableLed = false;
+        } else {
+            enableLed = true;
+        }
+
+        if (!enableLed) {
             mNotificationLight.turnOff();
         } else if (mNotificationPulseEnabled) {
             final Notification ledno = mLedNotification.sbn.getNotification();
